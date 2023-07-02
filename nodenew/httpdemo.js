1765f6d04d6c59153decc33ca05462bc0d5058d3
emir 1st <turkogluemir326@gmail.com>
2023-07-02 13:51:42 +0300

    done

--- nodenew/httpdemo.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-
-http.createServer((req, res) => {
-  res.write('hi world'/*başka bişi koy hesap makinen gibi*/)
-  //res.
-  res.end()
-}).listen(5000, () => console.log('server runnin like ezzz'));
