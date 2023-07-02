d1d4ce81620f57c23f2ae6bd58bd6e998ed8878f
emir 1st <turkogluemir326@gmail.com>
2023-07-02 15:42:57 +0300

    another one done

--- nodenew/fsdemo.js
+++ nodenew/fsdemo.js
@@ -1,13 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.writeFile(path.join(__dirname, '/test', 'hellowwwww.txt'), 'hihi wooorrrrllldddddssss', err => { /*'test' de olabilir*/
-  if (err) {
-    throw err
-  }
-  console.log('file done!!!');
-})
-
 //fs.writeFile(path.join(__dirname, '/test', 'hellowwwww.txt'), 'hihi wooorrrrllldddddssss', err => { /*'test' de olabilir ve farklara bak*/
 //  if (err) {
 //    throw err
