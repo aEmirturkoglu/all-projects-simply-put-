ec0a6d4dd7bb3e895cae76ef89c4b878d2e38367
emir 1st <turkogluemir326@gmail.com>
2023-07-02 15:44:28 +0300

    .

--- nodenew/fsdemo.js
+++ nodenew/fsdemo.js
@@ -1,17 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
-//fs.writeFile(path.join(__dirname, '/test', 'hellowwwww.txt'), 'hihi wooorrrrllldddddssss', err => { /*'test' de olabilir ve farklara bak*/
-//  if (err) {
-//    throw err
-//  }
-//  console.log('file done!!!');
-//
-//  fs.appendFile(path.join(__dirname, '/test', 'hellowwwww.txt'), ' hihi wooorrrrllldddddssss222', err => { /*'test' de olabilir*/
-//  if (err) {
-//    throw err
-//  }
-//  console.log('file done!!!');
-//})
-//
-//})
+fs.writeFile(path.join(__dirname, '/test', 'hellowwwww.txt'), 'hihi wooorrrrllldddddssss', err => { /*'test' de olabilir ve farklara bak*/
+ if (err) {
+   throw err
+ }
+ console.log('file done!!!');
+
+ fs.appendFile(path.join(__dirname, '/test', 'hellowwwww.txt'), ' hihi wooorrrrllldddddssss222', err => { /*'test' de olabilir*/
+ if (err) {
+   throw err
+ }
+ console.log('file done!!!');
+})
+
+})
