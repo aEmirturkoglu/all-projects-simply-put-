4dc614d8ea21f5a63f32745a32662530cd89daf2
emir 1st <turkogluemir326@gmail.com>
2023-07-02 15:48:09 +0300

    s

--- nodenew/os_demo.js
+++ nodenew/os_demo.js
@@ -1,9 +1,5 @@
 const os = require('os');
 
-//console.log(os.platform());
-//console.log(os.cpus());
-//console.log(os.freemem());
-//console.log(os.totalmem());
 console.log(os.arch());
 console.log(os.homedir());
 console.log(os.uptime());
