d5d2bcdd07d25c7c19b57e128a7474330d7cde68
emir 1st <turkogluemir326@gmail.com>
2023-07-02 13:51:42 +0300

    done

--- nodenew/fsdemo.js
+++ nodenew/fsdemo.js
@@ -31,18 +31,3 @@ const path = require('path');
 //})
 //
 //})
-
-//  fs.readFile(path.join(__dirname, '/test', 'hellowwwww.txt'), 'utf8', (err,data) => { /*'test' de olabilir*/
-//    if (err) {
-//      throw err
-//    }
-//    console.log(/*'file done!!!'*/ data);
-//  })
-
-fs.rename(path.join(__dirname, '/test', 'hellow22.txt'), 
-path.join(__dirname, '/test', 'hellow1.txt'), (err /*,data*/) => {
-if (err) {
-  throw err
-}
-console.log(/*data*/ 'renamed');
-})
