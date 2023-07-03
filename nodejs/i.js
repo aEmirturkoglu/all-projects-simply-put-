3632e78bbe54c60c6230e931b2c9304ce88a075d
emir 1st <turkogluemir326@gmail.com>
2023-07-03 16:47:19 +0300

    w

--- nodejs/i.js
+++ nodejs/i.js
@@ -1,6 +1,8 @@
 
-const fs = require('fs');
+const fsPromises = require('fs').promises;
 const path = require('path');
+
+
 
 // fs.readFile('./asd.txt', 'utf-8', (err , data) => {
 //   if (err) throw err;
@@ -13,9 +15,25 @@ fs.readFile(path.join(__dirname, /*'nodejs',*/ 'asd.txt'), 'utf-8', (err , data)
   console.log(data);
 });
 
-console.log('hello...');
+//console.log('hello...');
 
-fs.writeFile(path.join(__dirname, /*'nodejs',*/ 'asd.txt'), /*'utf-8',  bu zaten hazır kullanımda olacak*/ 'nice to meet ya', (err /*, data*/) => {
-  if (err) throw err;
-  console.log('writing done');
-});
+// fs.writeFile(path.join(__dirname, /*'nodejs',*/ 'reply.txt'), /*'utf-8',  bu zaten hazır kullanımda olacak*/ 'nice to meet ya', (err /*, data*/) => {
+//   if (err) throw err;
+//   console.log('writing done');
+
+//   fs.appendFile(path.join(__dirname, 'reply.txt'), '\n\nyes yes yes', (err) => {
+//     if (err) throw err;
+//     console.log( 'Append complete');
+
+//     fs.rename(path.join(__dirname, 'reply.txt'),path.join(__dirname, 'replied.txt'), (err) => {
+//       if (err) throw err;
+//       console.log( 'rename complete');
+//     });
+//   });
+// });
+
+// fs.appendFile(path.join(__dirname, 'test.txt'), 'Testing text.', (err) => {
+//   if (err) throw err;
+//   console.log( 'Append complete');
+// });
+  
