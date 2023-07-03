35d3c6b1f3f404f7102d743529b9dbc4e8f3a6ef
emir 1st <turkogluemir326@gmail.com>
2023-07-03 15:45:34 +0300

    w

--- nodejs/i.js
+++ nodejs/i.js
@@ -1,3 +1,21 @@
-const fs = require('fs');
 
-fs.readFile()
+const fs = require('fs');
+const path = require('path');
+
+// fs.readFile('./asd.txt', 'utf-8', (err , data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+// using path.join() is a much better approach
+fs.readFile(path.join(__dirname, /*'nodejs',*/ 'asd.txt'), 'utf-8', (err , data) => {
+  if (err) throw err;
+  console.log(data);
+});
+
+console.log('hello...');
+
+fs.writeFile(path.join(__dirname, /*'nodejs',*/ 'asd.txt'), /*'utf-8',  bu zaten hazır kullanımda olacak*/ 'nice to meet ya', (err /*, data*/) => {
+  if (err) throw err;
+  console.log('writing done');
+});
