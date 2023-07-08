29c5e64b21d01a6da48f4a800914c5686a043a54
emir 1st <turkogluemir326@gmail.com>
2023-07-08 13:14:53 +0300

    w

--- nodejs3/server.js
+++ nodejs3/server.js
@@ -1,3 +1,10 @@
+const http = require('http');
+const fs = require('fs');
+const fsPromises = fs.promises
+const path = require('path');
+
+
+
 const logEvents = require('./logEvents');
 
 const EventEmitter = require('events');
@@ -7,10 +14,49 @@ class MyEmitter extends EventEmitter {};
 // objyi başlat
 const myEmitter = new MyEmitter();
 
-// add listener for da log event myEmitter.on() and thats how we listen for an event
-myEmitter.on('log', (msg) => logEvents(msg));
+const PORT = process.env.PORT || 3500; 
 
-setTimeout(() => {
-  // emit event
-  myEmitter.emit('log', 'Log event emitted!')
-}, 2000);
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+
+    const extension = path.extname(req.url);
+
+    let contentType;
+
+    switch (extension) {
+      case '.css':
+          contentType = 'text/css';
+          break;
+      case '.js':
+          contentType = 'text/javascript';
+          break;
+      case '.json':
+          contentType = 'application/json';
+          break;
+      case '.jpg':
+          contentType = 'image/jpeg';
+          break;
+      case '.png':
+          contentType = 'image/png';
+          break;
+      case '.txt':
+          contentType = 'text/plain';
+          break;
+      default:
+          contentType = 'text/html';
+  }
+
+})
+
+server.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+})
+
+// // add listener for da log event myEmitter.on() and thats how we listen for an event
+// myEmitter.on('log', (msg) => logEvents(msg));
+
+// setTimeout(() => {
+//   // emit event
+//   myEmitter.emit('log', 'Log event emitted!')
+// }, 2000);
+
